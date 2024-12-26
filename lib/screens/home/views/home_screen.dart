d7faec7979b7e4aea8e7ca6a4f2f95a28adf116c
nishok19 <nishok1905@gmail.com>
2024-12-26 12:43:47 +0530

    Added charts from the library fl_charts and implemented in the stats page

--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:math';
 
+import 'package:expensetracker2/screens/add_expense/views/add_expense.dart';
 import 'package:expensetracker2/screens/home/views/main_screen.dart';
+import 'package:expensetracker2/screens/stats/stats.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +14,12 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  var widgetList = [MainScreen(), StatScreen()];
+
+  int index = 0;
+  late Color selectedItem = Colors.blue.shade400;
+  Color unselectedItem = Colors.grey;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,22 +27,40 @@ class _HomeScreenState extends State<HomeScreen> {
       bottomNavigationBar: ClipRRect(
         borderRadius: const BorderRadius.vertical(top: Radius.circular(30)),
         child: BottomNavigationBar(
-            onTap: (value) {},
+            onTap: (value) {
+              setState(() {
+                index = value;
+              });
+            },
             backgroundColor: Colors.white,
             showSelectedLabels: false,
             showUnselectedLabels: false,
             elevation: 3,
-            items: const [
+            items: [
               BottomNavigationBarItem(
-                  icon: Icon(CupertinoIcons.home), label: 'Home'),
+                  icon: Icon(
+                    CupertinoIcons.home,
+                    color: index == 0 ? selectedItem : unselectedItem,
+                  ),
+                  label: 'Home'),
               BottomNavigationBarItem(
-                  icon: Icon(CupertinoIcons.graph_circle_fill), label: 'Stats'),
+                  icon: Icon(
+                    CupertinoIcons.graph_circle_fill,
+                    color: index == 1 ? selectedItem : unselectedItem,
+                  ),
+                  label: 'Stats'),
             ]),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
         shape: const CircleBorder(),
-        onPressed: () {},
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute<void>(
+                builder: (BuildContext context) => const AddExpense()),
+          );
+        },
         child: Container(
             width: 60,
             height: 60,
@@ -47,7 +73,11 @@ class _HomeScreenState extends State<HomeScreen> {
                 ], transform: const GradientRotation(pi / 4))),
             child: const Icon(CupertinoIcons.add)),
       ),
-      body: const MainScreen(),
+      body: index == 0
+          ? MainScreen()
+          : index == 1
+              ? StatScreen()
+              : null,
     );
   }
 }
